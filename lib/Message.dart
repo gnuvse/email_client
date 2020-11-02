6d910710de2f4cad2884cce3a5ca8abd0a0509c6
Yury Iontsev <yiontsev@gmail.com>
2020-11-03 00:27:56 +0300

    Create method browse

--- lib/Message.dart
+++ lib/Message.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 
 part 'Message.g.dart';
 
@@ -11,4 +14,18 @@ class Message {
 
   factory Message.fromJson(Map<String, dynamic> json) =>
       _$MessageFromJson(json);
+
+  static Future<List<Message>> browse() async {
+    http.Response response = await http
+        .get('https://run.mocky.io/v3/cf90d6b8-9780-408d-9347-b8d29c96b5d5');
+
+    String content = response.body;
+
+    List collectionDataFromJson = json.decode(content);
+
+    List<Message> _messages =
+        collectionDataFromJson.map((json) => (Message.fromJson(json))).toList();
+
+    return _messages;
+  }
 }
