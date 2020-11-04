a68adaeb45378e77e0acad20b9361b604e7d8793
Yury Iontsev <yiontsev@gmail.com>
2020-11-04 11:57:47 +0300

    Add new page and button

--- lib/MessagesList.dart
+++ lib/MessagesList.dart
@@ -1,7 +1,8 @@
-import 'dart:convert';
-
+import 'package:email_clinet/ComposeButton.dart';
 import 'package:email_clinet/Message.dart';
 import 'package:flutter/material.dart';
+
+import 'MesageDeatail.dart';
 
 class MessagesList extends StatefulWidget {
   final String title;
@@ -21,56 +22,66 @@ class _MessagesListState extends State<MessagesList> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.refresh),
-                onPressed: () {
-                  var _messages = Message.browse();
-                  setState(() {
-                    messages = _messages;
-                  });
-                })
-          ],
-        ),
-        body: FutureBuilder(
-          future: messages,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-                return Center(child: CircularProgressIndicator());
-              case ConnectionState.done:
-                if (snapshot.hasError)
-                  return Text("There was an error: ${snapshot.error}");
-                var messages = snapshot.data;
-                return ListView.separated(
-                  separatorBuilder: (context, index) => Divider(),
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Message message = messages[index];
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.refresh_sharp),
+              onPressed: () {
+                var _messages = Message.browse();
+                setState(() {
+                  messages = _messages;
+                });
+              })
+        ],
+      ),
+      body: FutureBuilder(
+        future: messages,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              if (snapshot.hasError)
+                return Text("There was an error: ${snapshot.error}");
+              var messages = snapshot.data;
+              return ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Message message = messages[index];
 
-                    return ListTile(
-                      title: Text(message.subject),
-                      isThreeLine: true,
-                      leading: CircleAvatar(
-                        child: Text("PJ"),
-                        backgroundColor: Colors.red[500],
-                        foregroundColor: Colors.black,
-                      ),
-                      subtitle: Text(
-                        message.body,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    );
-                  },
-                );
-              default:
-            }
-          },
-        ));
+                  return ListTile(
+                    title: Text(message.subject),
+                    isThreeLine: true,
+                    leading: CircleAvatar(
+                      child: Text("PJ"),
+                      backgroundColor: Colors.red[500],
+                      foregroundColor: Colors.black,
+                    ),
+                    subtitle: Text(
+                      message.body,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              MessageDeatail(message.subject, message.body),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+          }
+        },
+      ),
+      floatingActionButton: ComposeButton(),
+    );
   }
 }
